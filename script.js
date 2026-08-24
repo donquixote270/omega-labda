fec10647e3c279342bf9f9f7ca82c871761ab2c7
donquixote270 <martins.pereira.victor@escola.pr.gov.br>
2026-08-24 11:49:57 -0300

    .

---
+++ script.js
@@ -0,0 +1,36 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const caixaResultado = document.querySelector('.texto-resultado');
+const listsa = [item1, item2]
+const perguntas = {
+tamanho: 20,
+tipo 'HB',
+cor: 'grafite',
+temBorrachaAtras: false
+}
+const perguntas = [
+{
+enunciado: "onde se localisa a peninsula de kola",
+Alternativas: [
+"russia",
+"estados unidos"
+] ,
+},
+{
+enunciado: "capital da polonia",
+Alternativas: [
+"danzig",
+"warsaw"
+],
+},
+];
+{
+enunciado: "baku se localisa em",
+Alternativas: [
+"georgia",
+"azerbaijan"
+],
+},
+];
